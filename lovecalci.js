345eef30b31af389282a197c9b4da6d03489f793
deepak2810 <deepakkhanka.97@gmail.com>
2022-03-04 23:53:02 +0530

    Javascript Foundation loading ..

---
+++ lovecalci.js
@@ -0,0 +1,9 @@
+console.log(" What is your name : ");
+console.log(" what is her/his name : ");
+
+var n = Math.random(n)*100;
+
+n = Math.floor(n)+1;
+
+
+console.log(" your love score is "+ " "+ n);
---
+++ randumnum.js
@@ -0,0 +1,10 @@
+// simple js program to for random number generator
+
+
+// Random number generation using Math.random() function.
+
+var n = Math.random()*6;
+
+ n = Math.floor(n);
+
+console.log(n);
