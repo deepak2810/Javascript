922c2e1a3455c14e3279c63564780e18efd3ec78
deepak2810 <deepakkhanka.97@gmail.com>
2022-03-20 18:00:44 +0530

    updated code

--- ADVANCE JAVASCRIPT AND DOM MANIPULATION/Drum Kit Starting Files/index.js
+++ ADVANCE JAVASCRIPT AND DOM MANIPULATION/Drum Kit Starting Files/index.js
@@ -1,18 +1,70 @@
+
+// Detecting keyboard press.
+
 var NumOfDrumButtons = document.querySelectorAll(".drum").length;
 
 
 // for adding eventListener to all of the our buttons.
-for (var i = 0; i<NumOfDrumButtons; i++){
+for (var i = 0; i < NumOfDrumButtons; i++) {
 
-document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
-  this.style.color = "blue";
+    var buttonInnnerHTML = this.innerHTML;
 
+    makeSound(buttonInnnerHTML);
 
-});
+  });
 
 }
 
 
-// var audio = new Audio('sounds/tom-1.mp3');
-// audio.play();
+// Derecting keyboard press.
+document.addEventListener("keypress",function(event){
+   makeSound(event.key);
+
+})
+
+function makeSound(key){
+  switch (key) {
+
+    case "w":
+      var audio1 = new Audio('sounds/tom-1.mp3');
+      audio1.play();
+      break;
+
+    case "a":
+      var audio2 = new Audio('sounds/tom-2.mp3');
+      audio2.play();
+      break;
+
+    case "s":
+      var audio3 = new Audio('sounds/tom-3.mp3');
+      audio3.play();
+      break;
+
+    case "d":
+      var audio4 = new Audio('sounds/tom-4.mp3');
+      audio4.play();
+      break;
+
+    case "j":
+      var audio5 = new Audio('sounds/snare.mp3');
+      audio5.play();
+      break;
+
+    case "k":
+      var audio6 = new Audio('sounds/crash.mp3');
+      audio6.play();
+      break;
+
+    case "l":
+      var audio7 = new Audio('sounds/kick-bass.mp3');
+      audio7.play();
+      break;
+
+
+    default:
+      console.log(buttonInnnerHTML);
+  }
+
+}
